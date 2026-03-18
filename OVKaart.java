b678f86baca2ff9553e0ce8fd0e59ff949ba81ba
DoakesdextermothaF <1217945@student.roc-nijmegen.nl>
2026-03-18 14:11:54 +0100

    EoD
    Een todo list van comments gezet met de taken die ik in mijn inchecken gaat uitvoeren. ik heb een functie gemaakt om voor inchecken voor de variabel incheckt om te checken van true of false en daarvoor heb ik een this statement gebruikt. En het meegeven van een double voor de variabel saldo. en nog bij de paal de if en else statements voor de variabelen van de boolean en nog de double.

--- OVKaart.java
+++ OVKaart.java
@@ -5,6 +5,12 @@ public class OVKaart {
 	private Double saldo;
 	private Boolean geldigheid;
 	private Boolean ingecheckt;
+	// to do:
+	// bedrag aftrekken van het saldo.
+	// meegeven van een double
+	
+	// functie maken die noemen we inchecken en die moet igecheckt van dat
+	// variabelen naar false en true.
 
 	// constructeur
 	public OVKaart(Double saldo, Boolean geldigheid, Boolean ingecheckt) {
@@ -15,6 +21,18 @@ public class OVKaart {
 
 	}
 
+	// Maak een methode inchecken voor de true en false.
+
+	public void inchecken() {
+		this.ingecheckt = true;
+	}
+	
+	//meegeven van een double
+	public void aftrekken(Double bedrag) {
+		this.saldo = this.saldo - bedrag;
+	}
+
+	// return waarde opvragen
 	public Double getSaldo() {
 		return saldo;
 	}
--- Paal.java
+++ Paal.java
@@ -3,15 +3,20 @@ package ReizenOpAnoniemOV;
 public class Paal {
 
 	private Double instapTarief;
-	
 
 	public void inChecken(OVKaart kaart) {
 
-		if (kaart.getSaldo() >= instapTarief) {
-		} else {
-			System.out.println("Je hebt te weinig saldo");
+		if (kaart.getGeldigheid() == true) {
+			if (kaart.getSaldo() >= instapTarief) {
+				kaart.aftrekken(instapTarief);
+				kaart.inchecken();
+				System.out.println("Je bent ingechekt");
+
+			} else {
+				System.out.println("Je bent niet ingechekckt");
+			}
+
 		}
 
 	}
-
 }
