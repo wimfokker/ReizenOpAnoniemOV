557b7837bc7b11d52ee518264d5d77cffd224b84
DoakesdextermothaF <1217945@student.roc-nijmegen.nl>
2026-04-15 14:14:37 +0200

    EoD
    Het maken van een automaat classe en het zetten voor opwaaderen van stop, scan, opwaaderenMet, en aanvullenTot.

---
+++ NsAutomaat.java
@@ -0,0 +1,21 @@
+package ReizenOpAnoniemOV;
+
+public class NsAutomaat {
+	
+	public void scan() {
+		
+	}
+	
+	public void opwaaderenMet() {
+		
+	}
+	
+	public void aanvullenTot() {
+		
+	}
+	
+	public void stop() {
+		
+	}
+
+}
